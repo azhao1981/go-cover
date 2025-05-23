f0aa17d0bb0ee08f2252d0469718f63957c36368
weiz <azhao.1981@gmail.com>
2025-05-23 13:56:59 +0800

    add py

--- cover.go
+++ cover.go
@@ -2,6 +2,8 @@ package cover
 
 import (
 	"strings"
+
+	"github.com/go-ego/gpy"
 )
 
 // 句子顺序覆盖率
@@ -53,4 +55,52 @@ func OrderCoverWithUnlessEnd(
 		sen2 = strings.Join(coverRate.Arr1[endIndex+1:], segTag)
 	}
 	return rate, sen1, sen2
+}
+
+// 顺序覆盖率，返回被覆盖的句子和句尾无效的句子，先转成拼音再比覆盖
+func OrderCoverWithPyUnlessEnd(
+	sentence1 string, sentence2 string, segTag string, minLen int,
+) (rate float64, sen1 string, sen2 string) {
+	arr1 := strings.Split(sentence1, segTag)
+	arr2 := strings.Split(sentence2, segTag)
+	if len(arr1) < minLen || len(arr2) < minLen {
+		return 0, "", sentence1
+	}
+	arr1Py := make([]string, len(arr1))
+	arr2Py := make([]string, len(arr2))
+	for i, v := range arr1 {
+		arr1Py[i] = HasToPY(v)
+	}
+	for i, v := range arr2 {
+		arr2Py[i] = HasToPY(v)
+	}
+	coverRate := &CoverRate{
+		Arr1: arr1Py,
+		Arr2: arr2Py,
+	}
+	coverRate.Count()
+	rate = coverRate.Arr2Rate()
+	if len(coverRate.HitArr1) == 0 {
+		return rate, "", sentence1
+	}
+	endIndex := coverRate.HitArr1[len(coverRate.HitArr1)-1][1]
+	sen1 = strings.Join(arr1[:endIndex+1], segTag)
+	if endIndex < len(arr1)-1 {
+		sen2 = strings.Join(arr1[endIndex+1:], segTag)
+	}
+	return rate, sen1, sen2
+}
+
+var GpyArgs = gpy.NewArgs()
+
+func HasToPY(sentence string) string {
+	py := gpy.Pinyin(sentence, GpyArgs)
+	if len(py) == 0 {
+		return ""
+	}
+	pyStr := make([]string, len(py))
+	for i, v := range py {
+		pyStr[i] = strings.Join(v, " ")
+	}
+	return strings.Join(pyStr, " ")
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/azhao1981/cover
 
 go 1.22.5
 
-require github.com/magiconair/properties v1.8.10
+require (
+	github.com/go-ego/gpy v0.42.1
+	github.com/magiconair/properties v1.8.10
+)
+
+require (
+	github.com/go-ego/gse v0.69.15 // indirect
+	github.com/vcaesar/cedar v0.20.0 // indirect
+)
